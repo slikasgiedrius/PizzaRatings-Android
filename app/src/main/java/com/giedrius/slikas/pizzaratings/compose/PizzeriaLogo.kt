487014f492d8acf60093e4f7fc2a609733724789
Giedrius Šlikas <slikas.giedrius@gmail.com>
2022-03-16 16:46:46 +0200

    Implement new coil image loader

--- app/src/main/java/com/giedrius/slikas/pizzaratings/compose/PizzeriaLogo.kt
+++ app/src/main/java/com/giedrius/slikas/pizzaratings/compose/PizzeriaLogo.kt
@@ -1,6 +1,5 @@
 package com.giedrius.slikas.pizzaratings.compose
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -11,18 +10,12 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.annotation.ExperimentalCoilApi
-import coil.compose.rememberImagePainter
+import coil.compose.AsyncImage
 
-@ExperimentalCoilApi
 @Composable
 fun PizzeriaLogo(logoUrl: String) {
-  Image(
-    painter = rememberImagePainter(
-      data = logoUrl,
-      builder = {
-        crossfade(true)
-      }
-    ),
+  AsyncImage(
+    model = logoUrl,
     contentDescription = "Pizzeria logo",
     contentScale = ContentScale.Crop,
     modifier = Modifier
