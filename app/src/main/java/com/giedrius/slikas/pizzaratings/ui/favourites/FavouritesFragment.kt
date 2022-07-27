179aac683737071a3f8f9a93494cbad7a0b03686
Giedrius Šlikas <slikas.giedrius@gmail.com>
2022-07-27 09:32:15 +0300

    update versions to dev (#5)
    * Update AGP version
    
    * Update a bunch of versions
    
    * Update compose versions and target/compile sdk versions to 33
    
    * Update google auth version
    
    * Update "Default" libraries versions

--- app/src/main/java/com/giedrius/slikas/pizzaratings/ui/favourites/FavouritesFragment.kt
+++ app/src/main/java/com/giedrius/slikas/pizzaratings/ui/favourites/FavouritesFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.material.Text
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
